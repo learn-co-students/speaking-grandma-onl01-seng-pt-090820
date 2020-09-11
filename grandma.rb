1960a99a85599267a2a6860266b99f77c8184065
Nattida Wong <nattidac.wong@gmail.com>
2020-09-11 13:32:33 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -11,3 +11,32 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(speak)
+  if speak == "Hi Nana, how are you?"
+    "huh?! speak up, sonny!".upcase
+  elsif speak == "Hi!"
+    "huh?! speak up, sonny!".upcase
+  elsif speak == "what did you eat today?".upcase
+    "no, not since 1938!".upcase
+  elsif speak == "WHAT?"
+    "NO, NOT SINCE 1938!"
+  else speak == "i love you grandma!".upcase
+    "i love you too pumpkin!".upcase
+  
+  
+  
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
