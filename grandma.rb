ad10d786c41c49b73eeebb16a7b3851ba11b4304
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-11 21:43:29 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,68 @@
-# Write a speak_to_grandma method.
+"# Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
+  # unless you shout it (type in all capitals).
+  
+  # If you shout, she can hear you (or at least she thinks so) 
+  # and yells back
+  
+  # NO, NOT SINCE 1938!
+  
+  # However if you say 'I LOVE YOU GRANDMA!', she should respond with
+  # 'I LOVE YOU TOO PUMPKIN!'
+  
+    it 'responds with HUH?! SPEAK UP, SONNY! for a second time' do
+    expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
+  end
 
-# If you shout, she can hear you (or at least she thinks so) 
-# and yells back
+  it 'responds with NO, NOT SINCE 1938! when she can hear you' do
+    expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')).to eq "NO, NOT SINCE 1938!"
+  end
 
-# NO, NOT SINCE 1938!
+  it 'responds with NO, NOT SINCE 1938! for a second time' do
+    expect(speak_to_grandma('WHAT?')).to eq "NO, NOT SINCE 1938!"
+  end
 
-# However if you say 'I LOVE YOU GRANDMA!', she should respond with
-# 'I LOVE YOU TOO PUMPKIN!'
+end
+
+describe '#speak_to_grandma' do
+  it 'responds with I LOVE YOU TOO PUMPKIN! if you say I LOVE YOU GRANDMA!' do
+    expect(speak_to_grandma('I LOVE YOU GRANDMA!')).to eq 'I LOVE YOU TOO PUMPKIN!'
+  end
+
+  it 'responds with HUH?! SPEAK UP, SONNY! unless you are shouting' do
+    expect(speak_to_grandma('Hi Nana, how are you?')).to eq 'HUH?! SPEAK UP, SONNY!'
+  end
+
+  it 'responds with HUH?! SPEAK UP, SONNY! for a second time' do
+    expect(speak_to_grandma('Hi!')).to eq 'HUH?! SPEAK UP, SONNY!'
+  end
+
+  it 'responds with NO, NOT SINCE 1938! when she can hear you' do
+    expect(speak_to_grandma('WHAT DID YOU EAT TODAY?')).to eq "NO, NOT SINCE 1938!"
+  end
+
+  it 'responds with NO, NOT SINCE 1938! for a second time' do
+    expect(speak_to_grandma('WHAT?')).to eq "NO, NOT SINCE 1938!"
+  end
+
+end
+
+
+def speak_to_grandma(spoken)
+  if spoken != spoken.upcase
+    puts "HUH, SPEAK UP SONNY
+  elsif spoken == spoken.upcase
+    puts "NO, NOT SINCE 1938!"
+   elsif spoken = "I LOVE YOU GRANDMA"
+    puts "I LOVE YOU TOO PUMPKIN!"
+    elsif spoken = "Hi!"
+      puts "HUH, SPEAK UP SONNY!"
+    elsif spoken = "WHAT DID YOU EAT TODAY?"
+    puts "NO NOT SINCE 1938!"
+  else puts "NO NOT SINCE 1938!"
+  end 
+end 
+
+  
