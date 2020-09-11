a694c93647f0895d8fecc8ba12de13219950c7f8
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-11 22:10:05 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -52,17 +52,17 @@
 
 def speak_to_grandma(spoken)
     if spoken != spoken.upcase
-      puts "HUH, SPEAK UP SONNY"
+      return "HUH?! SPEAK UP SONNY"
     elsif spoken == spoken.upcase
-      puts "NO, NOT SINCE 1938!"
+      return "NO, NOT SINCE 1938!"
     elsif spoken == "I LOVE YOU GRANDMA!"
-      puts "I LOVE YOU TOO PUMPKIN!"
+      return "I LOVE YOU TOO PUMPKIN!"
     elsif spoken == "Hi!"
-      puts "HUH, SPEAK UP SONNY!"
+      return "HUH, SPEAK UP SONNY!"
     elsif spoken == "WHAT DID YOU EAT TODAY?"
-      puts "NO NOT SINCE 1938!"
+      return "NO NOT SINCE 1938!"
     else 
-      puts "NO NOT SINCE 1938!"
+      return "NO NOT SINCE 1938!"
     end 
 end 
 
